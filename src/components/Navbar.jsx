bc6b18234284c4bedddea5db4ebaaef6da1e451c
Tanya <dougtanyah@gmail.com>
2025-01-18 10:15:35 -0500

    feat:Header component and initial setup of Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+//Header.js
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+//Navbar will have 
+export default function Navbar () {
+    return <nav classname="nav">
+
+    </nav>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+//StrictMode wraps project durng development to elp troubleshoot problems
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
