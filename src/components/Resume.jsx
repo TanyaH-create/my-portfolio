51e59819b3da2215180fd795e1217858819dac31
Tanya <dougtanyah@gmail.com>
2025-01-19 17:40:10 -0500

    feat/ resume component

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,6 +4,25 @@ function Resume() {
   return (
     <div>
       <h2>Resume</h2>
+      <br/>
+      <h2>Download my <a href='./src/assets/resume/resume.pdf'>resume</a></h2>
+      <h3> Front-end Proficiencies </h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>responsive design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+      <br/>
+      <h3>Back-end Proficiencies</h3>
+      <li>APIs</li>
+      <li>Node</li>
+      <li>Express</li>
+      <li>MySQL</li>
+      <li>REST</li>
+    
     </div>
   )
 }
