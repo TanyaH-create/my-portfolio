064f30b63710a1e4e64fcf01b43fe9abe9df96d6
Tanya <dougtanyah@gmail.com>
2025-01-19 14:45:12 -0500

    feat: removed LinkedIn text if footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
 
       <a href="https://www.linkedin.com/in/tanya-hauck-603895278/" target="_blank" rel="noopener noreferrer">
          <img src="./assets/images/LinkedInLogo.png" alt="Linked In Logo" className="logo" ></img>
-      LinkedIn</a>
+      </a>
 
       <a href="https://stackoverflow.com/users/29269726/tanyah" target="_blank" rel="noopener noreferrer">
          <img src="./assets/images/logo-stackoverflow.png" alt="Stack Overflow Logo" className="logo"></img>
